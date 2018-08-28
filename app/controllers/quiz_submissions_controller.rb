4ddea6d9c5a5e5d8794a2e19ad147297c055d192
Marc Freiman <marcfb2@hotmail.com>
2018-08-28 14:06:01 -0400

    render more than one profession with conditional logic

--- app/controllers/quiz_submissions_controller.rb
+++ app/controllers/quiz_submissions_controller.rb
@@ -2,8 +2,12 @@ class QuizSubmissionsController < ApplicationController
 
   def create
     ##figure out the career
-
-    redirect_to careers_path
+    if params[:question_7] == "20" #I like to work in groups
+       @career = [Career.find(16), Career.find(7)] #receptionisty
+       render 'careers/show'
+    else
+      redirect_to contact_path
+    end
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   get 'careers/index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 resources :questions
-resources :careers
+resources :careers, only: [:index]
   root 'questions#home'
   get 'contact' => 'contacts#index'
 
