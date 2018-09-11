833cda8aa6570a5344b1f511e7c3afb7ae708f79
Marc Freiman <marcfb2@hotmail.com>
2018-09-11 16:09:01 -0400

    added line to some config file

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -7,6 +7,7 @@ Rails.application.config.assets.version = '1.0'
 # Rails.application.config.assets.paths << Emoji.images_path
 # Add Yarn node_modules folder to the asset load path.
 Rails.application.config.assets.paths << Rails.root.join('node_modules')
+config.assets.precompile += Dir["#{Rails.root}/app/assets/stylesheets/site/site/*.*"].collect {|s| "site/" + File.basename(s).gsub(/.scss|.sass/, '') }
 
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in the app/assets
